304ebf36e85322a24138c5d74f3ada2c8b148648
malvfr <matheus.alvfr@gmail.com>
2020-04-03 13:18:13 -0300

    RNA Transcription

---
+++ protein-translation/lib/protein_translation.ex
@@ -0,0 +1,73 @@
+defmodule ProteinTranslation do
+  @proteins %{
+    "UGU" => "Cysteine",
+    "UGC" => "Cysteine",
+    "UUA" => "Leucine",
+    "UUG" => "Leucine",
+    "AUG" => "Methionine",
+    "UUU" => "Phenylalanine",
+    "UUC" => "Phenylalanine",
+    "UCU" => "Serine",
+    "UCC" => "Serine",
+    "UCA" => "Serine",
+    "UCG" => "Serine",
+    "UGG" => "Tryptophan",
+    "UAU" => "Tyrosine",
+    "UAC" => "Tyrosine",
+    "UAA" => "STOP",
+    "UAG" => "STOP",
+    "UGA" => "STOP"
+  }
+
+  @doc """
+  Given an RNA string, return a list of proteins specified by codons, in order.
+  """
+  @spec of_rna(String.t()) :: {atom, list(String.t())}
+  def of_rna(rna) do
+    data =
+      String.codepoints(rna)
+      |> Enum.chunk_every(3)
+      |> Enum.map(&Enum.join/1)
+
+    is_invalid = Enum.any?(data, fn c -> elem(of_codon(c), 0) == :error end)
+
+    if is_invalid do
+      {:error, "invalid RNA"}
+    else
+      {:ok,
+       Enum.take_while(data, fn c -> elem(of_codon(c), 1) !== "STOP" end)
+       |> Enum.map(fn c -> elem(of_codon(c), 1) end)}
+    end
+  end
+
+  @doc """
+  Given a codon, return the corresponding protein
+
+  UGU -> Cysteine
+  UGC -> Cysteine
+  UUA -> Leucine
+  UUG -> Leucine
+  AUG -> Methionine
+  UUU -> Phenylalanine
+  UUC -> Phenylalanine
+  UCU -> Serine
+  UCC -> Serine
+  UCA -> Serine
+  UCG -> Serine
+  UGG -> Tryptophan
+  UAU -> Tyrosine
+  UAC -> Tyrosine
+  UAA -> STOP
+  UAG -> STOP
+  UGA -> STOP
+  """
+  @spec of_codon(String.t()) :: {atom, String.t()}
+  def of_codon(codon) do
+    prot = @proteins[codon]
+
+    case prot do
+      nil -> {:error, "invalid codon"}
+      _ -> {:ok, prot}
+    end
+  end
+end
