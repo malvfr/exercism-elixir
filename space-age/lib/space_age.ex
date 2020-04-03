873f64a49de042d626f518b524d1a4dcaa169c7f
malvfr <matheus.alvfr@gmail.com>
2020-04-03 13:45:45 -0300

    Multiply in function instead of map

--- space-age/lib/space_age.ex
+++ space-age/lib/space_age.ex
@@ -10,15 +10,15 @@ defmodule SpaceAge do
           | :uranus
           | :neptune
 
-  @planet_year_seconds %{
-    earth: @earth_year_seconds,
-    mercury: @earth_year_seconds * 0.2408467,
-    venus: @earth_year_seconds * 0.61519726,
-    mars: @earth_year_seconds * 1.8808158,
-    jupiter: @earth_year_seconds * 11.862615,
-    saturn: @earth_year_seconds * 29.447498,
-    uranus: @earth_year_seconds * 84.016846,
-    neptune: @earth_year_seconds * 164.79132
+  @planet_earth_years %{
+    earth: 1,
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132
   }
 
   @doc """
@@ -27,6 +27,6 @@ defmodule SpaceAge do
   """
   @spec age_on(planet, pos_integer) :: float
   def age_on(planet, seconds) do
-    seconds / @planet_year_seconds[planet]
+    seconds / (@earth_year_seconds * @planet_earth_years[planet])
   end
 end
